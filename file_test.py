a06b80892167a298575f880cfb353f1be03a964b
Ubuntu <ubuntu@ip-172-31-32-235.eu-west-1.compute.internal>
2026-03-16 20:18:35 +0000

    correction flake8

--- file_test.py
+++ file_test.py
@@ -1,4 +1,4 @@
-import pytest
+
 
 def test_calc_addition():
     output = 2 + 4
@@ -6,18 +6,18 @@ def test_calc_addition():
 
 
 def test_calc_substraction():
-  # Fonction test du résultat de 2-4
+    # Fonction test du résultat de 2-4
     output = 2 - 4
     assert output == -2
 
 
 def test_calc_multiply():
-  # Fonction test du résultat de 2*4
+    # Fonction test du résultat de 2*4
     output = 2 * 4
     assert output == 8
 
 
 def test_coucou():
-  # Fonction test si le résultat renvoie 'hello'
-    output='hello'
+    # Fonction test si le résultat renvoie 'hello'
+    output = 'hello'
     assert output == 'hello'
